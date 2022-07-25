ce423df122b6bb1ecdd68878da54e1ca994ee861
tu7q <34932038+tu7q@users.noreply.github.com>
2022-07-25 21:32:06 +1200

    SRS utilities
    Started implementing the SRS functionality to the WebApp. Mostly organizational changes. Not in a stable state. Handling the database should come next

--- WebApp/SRS/utils.py
+++ WebApp/SRS/utils.py
@@ -1,36 +1,120 @@
 # Responsible for application logic
 from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum
 from django.contrib.sessions.backends.base import SessionBase
-from typing import Dict, Optional
+from typing import Dict, Literal, Optional, List, Type
+
+
+@dataclass
+class _Subject:
+    name: str
+    level: Literal[1, 2, 3]
+
+
+class Subject(str, Enum):
+    chemistry = _Subject(name="chemistry", level=3)
+    calculus = _Subject(name="calculus", level=3)
+    physics = _Subject(name="physics", level=3)
+
+
+@dataclass
+class _Assesment:
+    name: str
+    type: Literal["internal", "external"]
+
+
+class AssesmentBase(str, Enum):
+    _ignore_: str | list[str] = "all"
+    all: Dict[
+        Subject, AssesmentBase
+    ] = {}  # will not be discarded by the Enum meta class. (Only useful for type checker)
+
+    def __init_subclass__(cls, subject: Subject = None) -> None:
+        return super().__init_subclass__()
+        if subject is not None:
+            cls.all[subject] = cls
+
+    @classmethod
+    def subject_assesments(cls, subject: Subject) -> List[AssesmentBase]:
+        subject = cls.all[subject]
+        assesments = []
+        for assesment in subject:
+            assesments.append(assesment)
+        return assesment
+
+
+# Note:
+AssesmentBase.all = {}
+
+
+class ChemistryAssesments(AssesmentBase, subject=Subject.chemistry):
+    organic = _Assesment(name="organic", type="external")
+    spectroscopy = _Assesment(name="organic", type="external")
+    redux = _Assesment(name="organic", type="internal")
+
+
+class PhysicsAssesments(AssesmentBase, subject=Subject.physics):
+    waves = _Assesment(name="waves", type="external")
+    mechanics = _Assesment(name="mechanics", type="external")
+
+
+class CalculusAssesments(AssesmentBase, subject=Subject.calculus):
+    differentiation = _Assesment(name="differentiation", type="external")
+    integration = _Assesment(name="integration", type="external")
+    complex_algebra = _Assesment(name="complex algebra", type="external")
 
 
 class QuestionBase:
     """The base Subject class from which all questions derrive"""
 
-    all_questions: Dict[str, QuestionBase] = {}
+    """Should questions be stored or should they be generated through RNG(?)"""
 
-    def __init__(self) -> None:
-        self.filled_data = {}
-        self.mark = {}
+    all: Dict[AssesmentBase, List[Type[QuestionBase]]] = {}
 
+    def __init_subclass__(cls, assesment: AssesmentBase = None) -> None:
+        super().__init_subclass__()
+        if assesment is not None:
+            cls.all.setdefault(assesment, [])
+            cls.all[assesment].append(cls)
+
+    @classmethod
+    def assesment_questions(cls, assesment: AssesmentBase) -> Type[QuestionBase]:
+        """Gets all of the questions associated with an assesment"""
+        return cls.all[assesment]
+
+    def __init__(self, **kwargs) -> None:
+        pass
+
+    def _generate(self):
+        """Generates a new question"""
+        pass
+
+    def get_model_answer(self) -> Dict:
+        raise NotImplementedError("base class has no answers")
+
+    @classmethod
+    def from_json(cls, json) -> QuestionBase:
+        """Returns the object from JSON into an object"""
+        return cls(**json)
+
+    def json(self):
+        """turns the question into JSON for storage"""
+        raise NotImplementedError("base question class can't be stored")
+
+
+class OrganicChemistryQuestion(QuestionBase, assesment=ChemistryAssesments.organic):
     def _generate(self):
         pass
 
-    def __init_subclass__(cls, **kwargs) -> None:
-        super().__init_subclass__(**kwargs)
-        # QuestionBase.all_questions[cls.__name__] = cls
-        cls.all_questions[cls.__name__] = cls
+    def fill(self, data) -> None:
+        pass
 
-    def fill(self, data) -> None:  # POST data
-        raise NotImplementedError("Base class can't fill answers")
-
-    def mark(self) -> Dict:  # idk
-        # when being marked a
-        # self.__class__.__name__
-        raise NotImplementedError("base class can't be checked")
+    def mark(self) -> Dict:
+        pass
 
 
-class Chemistry(QuestionBase):
+class DifferntiationCalculusQuestion(QuestionBase, assesment=CalculusAssesments.differentiation):
     def _generate(self):
         pass
 
@@ -44,20 +128,20 @@ class Chemistry(QuestionBase):
 """ Other Questions """
 
 
-def all_subjects() -> bool:
-    return list(map(lambda x: x.lower(), QuestionBase.all_questions.keys()))
+# def all_subjects() -> bool:
+#     return list(map(lambda x: x.lower(), QuestionBase.questions.keys()))
 
 
-def get_or_create_question(subject: str, session: SessionBase) -> Optional[QuestionBase]:
-    """Assumes that subject is a valid subject."""
-    # try getting the question from the session
-    question = session.get(subject, None)
-    if question:
-        return question
-    # else create the session
-    question_type = QuestionBase.all_questions.get(subject)
-    # in this case it needs to access the queue mechanism and retreive the most recent element.
-    return question_type()
+# def get_or_create_question(subject: str, session: SessionBase) -> Optional[QuestionBase]:
+#     """Assumes that subject is a valid subject."""
+#     # try getting the question from the session
+#     question = session.get(subject, None)
+#     if question:
+#         return question
+#     # else create the session
+#     question_type = QuestionBase.questions.get(subject)
+#     # in this case it needs to access the queue mechanism and retreive the most recent element.
+#     return question_type()
 
 
 # logs in
--- WebApp/SRS/views.py
+++ WebApp/SRS/views.py
@@ -19,41 +19,46 @@ def Index(request: HttpRequest) -> HttpResponse:
 
 
 class Question(View):
-    # dispatcher: QuestionDispatcher = QuestionDispatcher()
-    valid_subjects: List[str] = utils.all_subjects()
+    # valid_subjects: List[str] = utils.all_subjects()
 
     # @RequireMSAuthentication
     def get(self, request: HttpRequest, subject: str = "") -> HttpResponse:
-        subject = subject.lower()
+        pass
+        # subject = subject.lower()
 
-        if subject not in Question.valid_subjects:
-            raise Http404
+        # if subject not in Question.valid_subjects:
+        #     raise Http404
 
-        if subject in request.session:
-            print("subject in session")
-            question = request.session[subject]
-        else:
-            print("subject not in session")
-            # models.next(subject)
-            question = "hi!"
-        request.session[subject] = question
+        # if subject in request.session:
+        #     print("subject in session")
+        #     question = request.session[subject]
+        # else:
+        #     print("subject not in session")
+        #     # models.next(subject)
+        #     question = "hi!"
+        # request.session[subject] = question
 
-        return HttpResponse(f"Hello There!")
+        # return HttpResponse(f"Hello There!")
         # return render("template", context={"question": question})
 
     # @RequireMSAuthentication
     def post(self, request: HttpRequest, subject: str = None) -> HttpResponse:
-        subject = subject.lower()
-        if subject not in Question.valid_subjects:
-            raise Http404
+        pass
+        # subject = subject.lower()
+        # if subject not in Question.valid_subjects:
+        #     raise Http404
 
-        # clear and retrieve the subject from the session
-        question = request.session.pop(subject, None)
-        if question is None:  # didn't make get request so subject and question pair don't exist.
-            raise HttpResponseBadRequest
-        question.fill(request.POST)  # fill the question with the users answers.
-        marked_question = question.mark()  # get the marked question
+        # # clear and retrieve the subject from the session
+        # question = request.session.pop(subject, None)
+        # if (
+        #     question is None
+        # ):  # didn't make get request so subject and question pair don't exist.
+        #     raise HttpResponseBadRequest
+        # question.fill(request.POST)  # fill the question with the users answers.
+        # marked_question = question.mark()  # get the marked question
 
-        models.return_to_queue(question)  # Question is not needed and is placed in the queue
+        # models.return_to_queue(
+        #     question
+        # )  # Question is not needed and is placed in the queue
 
-        return JsonResponse(marked_question)
+        # return JsonResponse(marked_question)
