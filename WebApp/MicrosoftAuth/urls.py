72c8ab81552f869292c1ef18a068fa4be137eb04
tu7q <34932038+tu7q@users.noreply.github.com>
2022-11-03 11:44:14 +1300

    small change

--- WebApp/MicrosoftAuth/urls.py
+++ WebApp/MicrosoftAuth/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("login/", views.Login, name="Login"),
-    path("callback/", views.Callback, name="Callback"),
-    path("logout/", views.Logout, name="Logout"),
+    path("login/", views.Login.as_view(), name="Login"),
+    path("callback/", views.Callback.as_view(), name="Callback"),
+    path("logout/", views.Logout.as_view(), name="Logout"),
 ]
--- WebApp/MicrosoftAuth/views.py
+++ WebApp/MicrosoftAuth/views.py
@@ -3,28 +3,33 @@ import logging
 from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth import logout
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.views import View
 
 from .authentication import authenticate
 from .authentication import BACKEND
 from .authentication import BACKEND_PATH
 
 
-def Login(request):
-    auth_uri = BACKEND.setup(request)
-    return render(request, "MicrosoftAuth/login.html", context={"auth_uri": auth_uri})
+class Login(View):
+    def get(self, request):
+        auth_uri = BACKEND.setup(request)
+        return render(request, "MicrosoftAuth/login.html", context={"auth_uri": auth_uri})
 
 
-def Callback(request):
-    user = authenticate(request)
-    if user:
-        login(request, user, backend=BACKEND_PATH)
-    # settings.INDEX -> EEK
-    return redirect(settings.LOGIN_REDIRECT_URL)
+class Callback(View):
+    def get(self, request):
+        user = authenticate(request)
+        if user:
+            login(request, user, backend=BACKEND_PATH)
+        # settings.INDEX -> EEK
+        return redirect(settings.LOGIN_REDIRECT_URL)
 
 
-def Logout(request):
-    logout(request)
-    # settings.INDEX -> EEK
-    return redirect(settings.LOGIN_URL)
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        # settings.INDEX -> EEK
+        return redirect(settings.LOGIN_URL)
